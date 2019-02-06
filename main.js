a3c3593f9010edd6f6ace26b3fdb639dd3b0b69b
Egor Nechaev <egornechaev91@gmail.com>
2019-02-06 18:04:51 +0200

    add obj rectRand

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-
-var currentPos = 0;
-const color = ["red","green","blue","white"];
 var score = 0;
 var isStart = false;
 var  continueAnimate = true;
- function random (min, max) {
-  let rand = min + Math.random() * (max + 1 - min);
-  rand = Math.floor(rand);
-  return rand;};
- 
+
 function Rect(){
+  this.currentPos = 0;
+  this.colorArr = ["red","green","blue","white"];
   this.rectH = 20;
   this.rectW = 20;
   this.postion = random(0,600);
   this.grav = random(2,5);
-  this.colorNumb = random(0,color.length);
+  this.colorNumb = random(0,this.colorArr.length);
+  this.setColor = ()=>this.colorArr[this.colorNumb];
+  function random (min, max) {
+    let rand = min + Math.random() * (max + 1 - min);
+    rand = Math.floor(rand);
+    return rand;};
+  
 }
 
-var rect1 = new Rect();
-
+var rectRand = new Rect();
 
 function animate() { 
   var scoreTable = document.getElementById('score');
@@ -28,9 +28,10 @@ function animate() {
     ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientWidth);
     return;
   } 
+
   isCursorRect = function (x,y){
-    return x > rect1.postion && x < rect1.postion + 40 &&
-           y > currentPos && y < currentPos + 50;
+    return x > rectRand.postion && x < rectRand.postion + 40 &&
+           y > rectRand.currentPos && y < rectRand.currentPos + 50;
   };
   canvas.onclick = function(e){
     var x = e.pageX;
@@ -38,23 +39,23 @@ function animate() {
     if(isCursorRect(x,y)){
       score+=1;
     scoreTable.innerHTML = score; 
-      currentPos = canvas.clientHeight;
+    rectRand.currentPos = canvas.clientHeight;
     }
   }
   ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientWidth);  
-  ctx.fillStyle = color[rect1.colorNumb];
-  ctx.fillRect(rect1.postion, currentPos, rect1.rectH, rect1.rectW);
-  currentPos += rect1.grav;
-  if(currentPos >= canvas.clientHeight) {
-    currentPos = 0;
-    rect1 = new Rect();
+  ctx.fillStyle = rectRand.setColor();
+  ctx.fillRect(rectRand.postion, rectRand.currentPos, rectRand.rectH, rectRand.rectW);
+  rectRand.currentPos += rectRand.grav;
+  if(rectRand.currentPos >= canvas.clientHeight) {
+    rectRand.currentPos = 0;
+    rectRand = new Rect();
   }
   requestAnimationFrame(animate);
 }
 function startGame(){ 
   if(!isStart){
     score = 0;
-    currentPos = 0;
+    rectRand = new Rect();
     continueAnimate = true;
     isStart = true;
     document.getElementById('score').innerHTML = score;
